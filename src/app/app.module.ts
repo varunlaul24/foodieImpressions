5207ee7b4d9396ec46c13756ae1a3f85d5b297bc
Varun Laul <varunlaul777@gmail.com>
2025-10-03 02:50:59 +0530

    Updated Angular dependencies in package.json to version 18.2.14 for core, Angular CLI and build tools to version 18.2.21. Replaced HttpClientModule with provideHttpClient and withInterceptorsFromDi in app.module.ts. Added esModuleInterop to tsconfig.json.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ChefListComponent } from './components/chef-list/chef-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ChefProfileComponent } from './components/chef-profile/chef-profile.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
-@NgModule({
-  declarations: [
-    AppComponent,
-    ChefListComponent,
-    HeaderComponent,
-    ChefProfileComponent,
-    HomeComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+@NgModule({ 
+    declarations: [
+        AppComponent,
+        ChefListComponent,
+        HeaderComponent,
+        ChefProfileComponent,
+        HomeComponent,
+        FooterComponent
+    ],
+    bootstrap: [AppComponent], 
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ], 
+    providers: [provideHttpClient(withInterceptorsFromDi())] 
 })
+
 export class AppModule { }
