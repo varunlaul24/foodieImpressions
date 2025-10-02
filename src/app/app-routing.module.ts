da574549b47245dc69080118732fed119ec71d03
Varun Laul <varunlaul777@gmail.com>
2025-10-03 03:27:34 +0530

    Migrated to lazy-loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChefListComponent } from './components/chef-list/chef-list.component';
-import { ChefProfileComponent } from './components/chef-profile/chef-profile.component';
-import { HomeComponent } from './components/home/home.component';
+
+
+
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'chefs', component: ChefListComponent },
-  { path: 'chefs/:id', component: ChefProfileComponent },
+  { path: '', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'chefs', loadComponent: () => import('./components/chef-list/chef-list.component').then(m => m.ChefListComponent) },
+  { path: 'chefs/:id', loadComponent: () => import('./components/chef-profile/chef-profile.component').then(m => m.ChefProfileComponent) },
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
